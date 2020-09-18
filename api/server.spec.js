09ddb2594e852e72efdfba6a6e6b88ec7ae0b4e3
megawatts68 <andrew.f.watts@gmail.com>
2020-09-19 02:56:15 +0700

    4tests working

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,12 +3,20 @@ const supertest = require("supertest")
 const server = require("./server.js")
 const db = require("../database/dbConfig.js")
 
-describe('POST /Register', () => {
-    beforeEach(async () => {
-        await db("users").truncate();
-    });
+describe('POST /auth', () => {
+    // beforeEach(async () => {
+    //     await db("users").truncate();
+    // });
+    it("/register should return 400 when passed bad data", () => {
+            return supertest(server)
+                .post("/api/auth/register")
+                .send({ user: "user", pass: "password" })
+                .then(res => {
+                    expect(res.status).toBe(400);
+                });
+        })
 
-    it("should return 201 when passed correct data", () => {
+    it("/register should return 201 when passed correct data", () => {
         return supertest(server)
             .post("/api/auth/register")
             .send({ username: "radmosol", password: "password" })
@@ -17,12 +25,34 @@ describe('POST /Register', () => {
             });
     });
 
-    it("should return 400 when passed bad data", () => {
+    it("/login should return 200 when passed correct data", () => {
         return supertest(server)
-            .post("/api/auth/register")
-            .send({ user: "user", password: "password" })
+            .post("/api/auth/login")
+            .send({ username: "radmosol", password: "password" })
             .then(res => {
-                expect(res.status).toBe(400);
+                expect(res.status).toBe(200);
             });
+    });
+
+    
+    it("/login should return 401 when passed bad creds", () => {
+        return supertest(server)
+            .post("/api/auth/login")
+            .send({ username: "radmosol", password: "wrongpassword" })
+            .then(res => {
+                expect(res.status).toBe(401);
+            });
+    })
+})
+
+describe('GET /jokes', () => {
+    it("should return 401 if not logged in", async () => {
+        const res = await supertest(server).get("/api/jokes")
+            expect(res.body).toBe({message: "No token!"})
+    })
+
+    it("should return jokes if logged in", async () => {
+        const res = await supertest(server).get("/api/jokes")
+            expect(res.body.joke).toBe(true)
     })
 })
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -39,7 +39,7 @@ router.post('/login', (req, res) => {
             .then(([user]) => {
                 if (user && bcryptjs.compareSync(password, user.password)) {
                     const token = makeJwt(user);
-
+                  
                     res.status(200).json({ token });
                 } else {
                     res.status(401).json({ message: "Invalid credentials" });
